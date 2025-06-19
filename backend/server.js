89247ff32229a509acdfa589d62c26f65581889b
kHUSHI SINGH <khushisingh0127@gmail.com>
2025-06-19 21:09:26 +0530

    Refined

--- backend/server.js
+++ backend/server.js
@@ -10,28 +10,30 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-//Middleware to handle CORS
+// Middleware to handle CORS
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://expense-tracker-frontend-k8lq.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
 
 connectDB();
 
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/income",incomeRoutes);
-app.use("/api/v1/expense",expenseRoutes);
-app.use("/api/v1/dashboard",dashboardRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
-//server uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
-
+// serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
