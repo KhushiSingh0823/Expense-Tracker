ae6bd9f05a7f2d44c0fe203fe7a0403feca4eeb8
kHUSHI SINGH <khushisingh0127@gmail.com>
2025-06-20 22:03:32 +0530

    msg

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,41 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-//Middleware to handle CORS
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+const corsOptions = {
+   origin: [
+    "http://localhost:5173",
+    "https://expense-tracker-hqdkye4xs-khushisingh0823s-projects.vercel.app",
+    "https://expense-tracker-mauve-three-18.vercel.app"
+  ],
 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+// Parse JSON bodies
 app.use(express.json());
 
+// Connect to MongoDB
 connectDB();
 
-app.use("/api/v1/auth",authRoutes);
+// API routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
-//server uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+// Serve uploads (e.g., profile images)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
